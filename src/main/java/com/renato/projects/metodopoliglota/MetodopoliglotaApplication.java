d4fde431ab3dc69ff0745bfb3d6ababf1113c333
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-11-28 11:39:41 -0300

    project set up

---
+++ src/main/java/com/renato/projects/metodopoliglota/MetodopoliglotaApplication.java
@@ -0,0 +1,13 @@
+package com.renato.projects.metodopoliglota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodopoliglotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodopoliglotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renato/projects/metodopoliglota/MetodopoliglotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.projects.metodopoliglota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetodopoliglotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
